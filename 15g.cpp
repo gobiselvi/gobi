5ce15fbeb5d678e00ede077f822292eac9dd9ff9
gobiselvi <31535397+gobiselvi@users.noreply.github.com>
2017-09-03 13:54:08 +0530

    Create 15g.cpp

---
+++ 15g.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int low, high, n, flag;
+
+    cout << "Enter two numbers(intervals): ";
+    cin >> low >> high;
+
+    cout << "Prime numbers between " << low << " and " << high << " are: ";
+
+    while (low < high)
+    {
+        flag = 0;
+
+        for(n = 2; n <= low/2; ++n)
+        {
+            if(low % n == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            cout << low << " ";
+
+        ++low;
+    }
+
+    return 0;
+}
