792848bbc20d646465950ba9f2c2be327befc788
gobiselvi <31535397+gobiselvi@users.noreply.github.com>
2017-09-03 12:16:31 +0530

    Create 4a.c

---
+++ 4a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char g;
+    printf("Enter a character: ");
+    scanf("%c",&g);
+
+    if( (g>='a' && g<='z') || (g>='A' && g<='Z'))
+        printf("%c is an alphabet.",g);
+    else
+        printf("%c is not an alphabet.",g);
+
+    return 0;
+}
